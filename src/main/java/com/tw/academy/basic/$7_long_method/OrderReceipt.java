72dcf1c7f30da5df5f9216d439546586d4f3a117
ojf1996 <247057911@qq.com>
2021-06-25 21:21:17 +0800

    [jimmy]fix: remove magic number and magic string

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -8,6 +8,12 @@ package com.tw.academy.basic.$7_long_method;
  * @since   2018-1-1
  */
 public class OrderReceipt {
+    public static final String HEADER = "======Printing Orders======\n";
+    public static final char TAB = '\t';
+    public static final char NEW_LINE = '\n';
+    public static final String SALES_TAX_LABEL = "Sales Tax";
+    public static final String TOTAL_AMOUNT_LABEL = "Total Amount";
+    public static final double TAX_RATE = .10;
     private Order order;
 
     public OrderReceipt(Order order) {
@@ -17,7 +23,7 @@ public class OrderReceipt {
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        output.append("======Printing Orders======\n");
+        output.append(HEADER);
 
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
@@ -26,23 +32,23 @@ public class OrderReceipt {
         double totalSalesAmount = 0d;
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
-            output.append('\t');
+            output.append(TAB);
             output.append(lineItem.getPrice());
-            output.append('\t');
+            output.append(TAB);
             output.append(lineItem.getQuantity());
-            output.append('\t');
+            output.append(TAB);
             output.append(lineItem.totalAmount());
-            output.append('\n');
+            output.append(NEW_LINE);
 
-            double salesTax = lineItem.totalAmount() * .10;
+            double salesTax = lineItem.totalAmount() * TAX_RATE;
             totalSalesTaxAmount += salesTax;
 
             totalSalesAmount += lineItem.totalAmount() + salesTax;
         }
 
-        output.append("Sales Tax").append('\t').append(totalSalesTaxAmount);
+        output.append(SALES_TAX_LABEL).append(TAB).append(totalSalesTaxAmount);
 
-        output.append("Total Amount").append('\t').append(totalSalesAmount);
+        output.append(TOTAL_AMOUNT_LABEL).append(TAB).append(totalSalesAmount);
         return output.toString();
     }
 }
