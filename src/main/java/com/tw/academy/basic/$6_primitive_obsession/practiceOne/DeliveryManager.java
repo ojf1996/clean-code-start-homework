d8347a623f881bbdb83e0c5d6f134e18e8e54530
jimmy ou <jimmy.ou@oocl.com>
2021-06-25 15:48:09 +0800

    remove primitive type for toAddress and fromAddress

--- src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
+++ src/main/java/com/tw/academy/basic/$6_primitive_obsession/practiceOne/DeliveryManager.java
@@ -3,21 +3,17 @@ package com.tw.academy.basic.$6_primitive_obsession.practiceOne;
 public class DeliveryManager {
     private final Address fromAddress1;
     private final Address toAddress1;
-    String toAddress;
-    String fromAddress;
 
     public DeliveryManager(Address toAddress, Address fromAddress) {
         fromAddress1 = fromAddress;
-        this.toAddress = fromAddress1.getFromAddress();
         toAddress1 = toAddress;
-        this.fromAddress = toAddress1.getFromAddress();
     }
 
     public DeliverCenter allocate(){
-        if (getProvince(toAddress).equals(getProvince(fromAddress)) && getCity(toAddress).equals(getCity(fromAddress))){
+        if (getProvince(fromAddress1.getFromAddress()).equals(getProvince(toAddress1.getFromAddress())) && getCity(fromAddress1.getFromAddress()).equals(getCity(toAddress1.getFromAddress()))){
             return DeliverCenter.LOCAL;
         }
-        if (getProvince(toAddress).equals(getProvince(fromAddress))) {
+        if (getProvince(fromAddress1.getFromAddress()).equals(getProvince(toAddress1.getFromAddress()))) {
             return DeliverCenter.PROVINCE;
         }
         return DeliverCenter.FOREIGN;
