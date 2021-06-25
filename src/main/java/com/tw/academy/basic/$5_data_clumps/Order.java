5311e822928f39d3661a32d12ea496b06d7f31b4
jimmy ou <jimmy.ou@oocl.com>
2021-06-25 14:53:10 +0800

    inline field buyer pnone num to buyer

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -4,20 +4,18 @@ public class Order {
 
     private int orderNumber;
 
-    private String buyerPhoneNumber;
     private String buyerAddress;
     private Buyer buyer;
 
     public Order(int orderNumber, Buyer buyer) {
         this.orderNumber = orderNumber;
         this.buyer = buyer;
-        this.buyerPhoneNumber = buyer.getBuyerPhoneNumber();
         this.buyerAddress = buyer.getBuyerAddress();
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyer.getBuyerName(), buyerPhoneNumber, buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", buyer.getBuyerName(), buyer.getBuyerPhoneNumber(), buyerAddress);
     }
 }
 
