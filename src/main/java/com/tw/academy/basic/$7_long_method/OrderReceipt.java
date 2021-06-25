c62be6a6bda20d6dc4cd63788bea5e60c52d8e11
ojf1996 <247057911@qq.com>
2021-06-25 20:47:46 +0800

    [jimmy]fix: delete dead code & unused comment.

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -14,25 +14,14 @@ public class OrderReceipt {
         this.o = o;
     }
 
-    //Deprecated
-    public String printCustomerName() {
-        return o.getCustomerName();
-    }
-
-    //todo: rename -- Tom
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
 
-        // print headers
         output.append("======Printing Orders======\n");
 
-        // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
         output.append(o.getCustomerName());
         output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
 
-        // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
         for (LineItem lineItem : o.getLineItems()) {
@@ -45,18 +34,14 @@ public class OrderReceipt {
             output.append(lineItem.totalAmount());
             output.append('\n');
 
-            // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
             totSalesTx += salesTax;
 
-            // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
         }
 
-        // prints the state tax
         output.append("Sales Tax").append('\t').append(totSalesTx);
 
-        // print total amount
         output.append("Total Amount").append('\t').append(tot);
         return output.toString();
     }
