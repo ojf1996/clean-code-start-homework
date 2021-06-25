df9b2e2b5a5d652374bfe47b01dc18356f731632
ojf1996 <247057911@qq.com>
2021-06-25 21:25:02 +0800

    [jimmy]fix: extract printReceipt to Order class

--- src/main/java/com/tw/academy/basic/$7_long_method/Order.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/Order.java
@@ -24,4 +24,36 @@ public class Order {
     public List<LineItem> getLineItems() {
         return lineItems;
     }
+
+    String printOrder(String header, char tab, char newLine, String salesTaxLabel, String totalAmountLabel) {
+        StringBuilder output = new StringBuilder();
+
+        output.append(header);
+
+        output.append(getCustomerName());
+        output.append(getCustomerAddress());
+
+        double totalSalesTaxAmount = 0d;
+        double totalSalesAmount = 0d;
+        for (LineItem lineItem : getLineItems()) {
+            output.append(lineItem.getDescription());
+            output.append(tab);
+            output.append(lineItem.getPrice());
+            output.append(tab);
+            output.append(lineItem.getQuantity());
+            output.append(tab);
+            output.append(lineItem.totalAmount());
+            output.append(newLine);
+
+            double salesTax = lineItem.totalAmount() * OrderReceipt.TAX_RATE;
+            totalSalesTaxAmount += salesTax;
+
+            totalSalesAmount += lineItem.totalAmount() + salesTax;
+        }
+
+        output.append(salesTaxLabel).append(tab).append(totalSalesTaxAmount);
+
+        output.append(totalAmountLabel).append(tab).append(totalSalesAmount);
+        return output.toString();
+    }
 }
--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -21,34 +21,13 @@ public class OrderReceipt {
     }
 
     public String printReceipt() {
-        StringBuilder output = new StringBuilder();
-
-        output.append(HEADER);
-
-        output.append(order.getCustomerName());
-        output.append(order.getCustomerAddress());
-
-        double totalSalesTaxAmount = 0d;
-        double totalSalesAmount = 0d;
-        for (LineItem lineItem : order.getLineItems()) {
-            output.append(lineItem.getDescription());
-            output.append(TAB);
-            output.append(lineItem.getPrice());
-            output.append(TAB);
-            output.append(lineItem.getQuantity());
-            output.append(TAB);
-            output.append(lineItem.totalAmount());
-            output.append(NEW_LINE);
-
-            double salesTax = lineItem.totalAmount() * TAX_RATE;
-            totalSalesTaxAmount += salesTax;
-
-            totalSalesAmount += lineItem.totalAmount() + salesTax;
-        }
-
-        output.append(SALES_TAX_LABEL).append(TAB).append(totalSalesTaxAmount);
-
-        output.append(TOTAL_AMOUNT_LABEL).append(TAB).append(totalSalesAmount);
-        return output.toString();
+        String header = HEADER;
+        Order order = this.order;
+        char tab = TAB;
+        char newLine = NEW_LINE;
+        String salesTaxLabel = SALES_TAX_LABEL;
+        String totalAmountLabel = TOTAL_AMOUNT_LABEL;
+        return order.printOrder(header, tab, newLine, salesTaxLabel, totalAmountLabel);
     }
+
 }
